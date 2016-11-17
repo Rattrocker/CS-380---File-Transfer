cb3ae4d34340a35ff0385190e2f265fc420e4051
Christian Hill <Christian.T.Hill@jpl.nasa.gov>
2016-11-16 19:05:39 -0800

    Added basic argument parsing and socket connections

---
+++ Project/src/TransferServer.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by cthill on 11/16/16.
+ */
+public class TransferServer {
+    protected ServerSocket serverSocket;
+
+    public TransferServer(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+    }
+
+    public void serve() throws IOException {
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connect: " + clientSocket.getInetAddress());
+            BufferedReader socketIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            DataOutputStream socketOut = new DataOutputStream(clientSocket.getOutputStream());
+
+            //do stuff here
+        }
+    }
+}
